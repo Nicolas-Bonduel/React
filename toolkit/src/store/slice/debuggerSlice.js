ac43b13e25f845c1c5d884bcfc2dd1ab7c497605
Nicolas-Bonduel <146720489+Nicolas-Bonduel@users.noreply.github.com>
2024-04-25 10:05:24 +0200

    Add files via upload

---
+++ toolkit/src/store/slice/debuggerSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const debuggerSlice = createSlice({
+    name: 'debugger',
+    reducers: {
+        add(state, action) {
+            state.actions.push({date: new Date().toString(), action: action.payload});
+        },
+        reset(state, action) {
+            state.actions = [];
+        }
+    },
+    initialState: {
+        actions: []
+    }
+});
+
+export default debuggerSlice.reducer;
+
+export const {
+    add,
+    reset
+} = debuggerSlice.actions;
+
+export const actions = debuggerSlice.actions;
---
+++ toolkit/src/store/slice/todolistSlice.js
@@ -0,0 +1,83 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const todolistSlice = createSlice({
+    name: 'todolist',
+
+    initialState: {
+        loading: false,
+        todolist: []
+        /* list of
+             - id
+             - title
+             - description
+             - checked
+        */
+    },
+    reducers: {
+        add(state, action) {
+            state.todolist.push({
+                id: Date.now(),
+                title: action.payload.title,
+                description: action.payload.desc,
+                checked: false,
+            });
+            state.loading = false;
+        },
+        setChecked(state, action) {
+            state.todolist.find((t) => t.id == action.payload.id).checked = action.payload.checked;
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(addTodo.pending, (state, action) => {
+            console.log('pending');
+            console.log(state);
+            console.log(action);
+            state.loading = true;
+        })
+            .addCase(addTodo.rejected, (state, action) => {
+                console.log('rejected');
+                console.log(state);
+                console.log(action);
+                state.loading = false;
+            })
+            .addCase(addTodo.fulfilled, (state, action) => {
+                console.log('fulfilled');
+                console.log(state);
+                console.log(action);
+                state.todolist.push({
+                    id: Date.now(),
+                    title: action.meta.arg.title,
+                    description: action.meta.arg.desc,
+                    checked: false,
+                });
+                state.loading = false;
+            })
+    }
+});
+
+export const addTodo = createAsyncThunk(
+    'addTodo',
+    async (title, desc) => {
+        await new Promise(res => setTimeout(res, 1000));
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({
+                title: title,
+                body: desc,
+                userId: -999999999999999999999999999999999999999,
+            })
+        })
+        return res.data;
+    }
+);
+
+
+export default todolistSlice.reducer;
+
+export const {
+    add,
+    setChecked
+} = todolistSlice.actions;
